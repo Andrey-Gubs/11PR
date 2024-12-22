22d8b53132f86efaa0f932b7cdb10d68ce111be3
Andrey Gubskiy <andrej.gubskiy@gmail.com>
2024-12-22 21:11:50 +0300

    11pr2
    11.2pr

---
+++ DateComparison.java
@@ -0,0 +1,36 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class DateComparison {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Получаем текущую дату
+        LocalDate currentDate = LocalDate.now();
+        System.out.println("Текущая дата: " + currentDate);
+
+        // Дата ввод в формате ГГГГ-ММ-ДД
+        System.out.print("Введите дату: ");
+        String userInput = scanner.nextLine();
+
+        try {
+
+            LocalDate enteredDate = LocalDate.parse(userInput, DateTimeFormatter.ISO_LOCAL_DATE);
+
+
+            if (enteredDate.isBefore(currentDate)) {
+                System.out.println("Ваша дата " + enteredDate + " раньше текущей даты.");
+            } else if (enteredDate.isAfter(currentDate)) {
+                System.out.println("Ваша дата " + enteredDate + " позже текущей даты.");
+            } else {
+                System.out.println("Ваша дата равна текущей дате!");
+            }
+        } catch (DateTimeParseException e) {
+            System.out.println("Введен неверный формат даты. Пожалуйста, используйте формат ГГГГ-ММ-ДД.");
+        } finally {
+            scanner.close();
+        }
+    }
+}
